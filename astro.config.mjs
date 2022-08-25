8dc447addf806fa41dfb9bc80f620272cd4c2376
rzepaw <rzepaw@gmail.com>
2022-08-25 13:54:19 +0200

    defineConfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import preact from '@astrojs/preact';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [preact()],
+	site: 'https://instap-app.github.io',
+	base: '/website-definition'
 });
-
-site: 'https://instap-app.github.io/website-definition/'
-base: '/website-definition'
